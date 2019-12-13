350ed94c7a11b32f47ac7ae4bcc2081826b08784
Strykar1 <ahmedms.ftw@gmail.com>
2019-12-13 20:15:53 +0200

    feat: added arrival time to processes

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,11 @@
-queue1 = []
-queue2 = []
-queue3 = []
+from collections import deque
+import math
+
+level0 = deque()
+level1 = deque()
+FCFS = deque()
 processes = []
+queues = [level0, level1, FCFS]
 
 
 class Process:
@@ -9,67 +13,101 @@ class Process:
     counter = 0
     t = 0
 
-    def __init__(self, burst):
+    def __init__(self, arrival, burst):
         self.waiting = 0
+        self.arrival = arrival
         self.burst = burst
         self.remB = self.burst
         self.isFinished = False
         Process.counter += 1
         self.id = Process.counter
         self.finish = 0
+        self.level = 0
+        processes.append(self)
 
     def print(self):
-        print("process " + str(self.id) + '\t\t' + str(self.burst) + '\t\t' + str(self.waiting) + '\t\t' + str(self.waiting + self.burst))
+        print("process " + str(self.id) + " \t(" + str(self.arrival) + ") " + '\t\t\t\t' + str(self.burst) + '\t\t' + str(self.waiting) + '\t\t' + str(self.finish - self.arrival))
 
     def wait(self):
-        self.waiting = self.finish - self.burst
+        self.waiting = self.finish - self.arrival - self.burst
+
+    def assign(self):
+        queues[self.level].append(self)
+
+    def promote(self):
+        if not self.isFinished:
+            if self.level + 1 < len(queues):
+                self.level += 1
+                self.assign()
 
 
-def roundRobin(queue, quantum):
-    for p in queue:
-        Process.t += min(p.remB, quantum)
-        p.remB = max(p.remB - quantum, 0)
+def roundRobin(level, quantum):
+    ar = checkNextArrival()
+    checkProcessesLevels()
+    for p in (queues[level]):
+        if p.level == level:
+            if Process.t + p.remB > ar and not level == 0:
+                p.remB -= (ar - Process.t)
+                execute(0)
+                break
+            Process.t += min(p.remB, quantum)
+            p.remB = max(p.remB - quantum, 0)
+            if p.remB <= 0:
+                p.isFinished = True
+                p.finish = Process.t
+            p.promote()
+    queues[level].clear()
+    execute(level + 1)
 
-        if p.remB <= 0:
-            p.isFinished = True
-            p.finish = Process.t
 
-
-def promote(queueL, queueH):
-    for p in queueL:
-        if not p.isFinished:
-            queueH.append(p)
+def checkProcessesLevels():
+    for p in processes:
+        if p.arrival <= Process.t and not queues[p.level].__contains__(p):
+            queues[p.level].append(p)
 
 
 def fcfs():
-    for p in queue3:
+    ar = checkNextArrival()
+    for p in queues[len(queues) - 1]:
+        if Process.t + p.burst > ar:
+            p.remB -= (ar - Process.t)
+            execute(0)
+            break
         Process.t += p.remB
         p.remB = 0
         p.isFinished = True
         p.finish = Process.t
 
 
+def execute(level):
+    level = max(0, level)
+    if level < len(queues) - 1:
+        roundRobin(level, 8 + level * 8)
+    else:
+        fcfs()
+
+
 def printAll():
-    print("Process no" + '\t\t' + "Burst " + " Wait " + "Turnaround" + "\n")
+    print("\n"+"Process no" + " (Arrival time) " + '\t\t' + "Burst " + " Wait " + "Turnaround")
     for p in processes:
         p.wait()
         p.print()
 
 
+def checkNextArrival():
+    for p in processes:
+        if p.arrival > Process.t:
+            return p.arrival
+    return math.inf
+
+
 def test():
-    p1 = Process(9)
-    p2 = Process(26)
-    p3 = Process(10)
-    p4 = Process(6)
+    Process(25, 9)
+    Process(0, 26)
+    Process(3, 10)
+    Process(9, 6)
 
-    processes.extend([p1, p2, p3, p4])
-
-    promote(processes, queue1)
-    roundRobin(queue1, 8)
-    promote(queue1, queue2)
-    roundRobin(queue2, 16)
-    promote(queue2, queue3)
-    fcfs()
+    execute(0)
 
     printAll()
 
