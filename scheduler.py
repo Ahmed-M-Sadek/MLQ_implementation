89468f2de8f4489657ec229af95f2ca71b9b8e70
Strykar1 <ahmedms.ftw@gmail.com>
2019-12-13 23:03:57 +0200

    MLFQ reworked implementaion and less bugs :)

--- scheduler.py
+++ scheduler.py
@@ -1,17 +1,19 @@
 from collections import deque
 import math
 
-level0 = deque()
-level1 = deque()
-FCFS = deque()
-processes = []
-queues = [level0, level1, FCFS]
+
+class queue:
+
+    def __init__(self, quantum):
+        self.quantum = quantum
+        self.queue = deque()
 
 
 class Process:
 
     counter = 0
     t = 0
+    nextArrival = None
 
     def __init__(self, arrival, burst):
         self.waiting = 0
@@ -23,6 +25,7 @@ class Process:
         self.id = Process.counter
         self.finish = 0
         self.level = 0
+        self.q = 0
         processes.append(self)
 
     def print(self):
@@ -34,71 +37,73 @@ class Process:
     def assign(self):
         queues[self.level].append(self)
 
-    def promote(self):
+    def demote(self):
         if not self.isFinished:
             if self.level + 1 < len(queues):
                 self.level += 1
                 self.assign()
 
 
-def roundRobin(level, quantum):
-    ar = checkNextArrival()
-    checkProcessesLevels()
-    for p in (queues[level]):
-        if p.level == level:
-            if Process.t + p.remB > ar and not level == 0:
-                p.remB -= (ar - Process.t)
-                execute(0)
-                break
-            Process.t += min(p.remB, quantum)
-            p.remB = max(p.remB - quantum, 0)
-            if p.remB <= 0:
-                p.isFinished = True
-                p.finish = Process.t
-            p.promote()
-    queues[level].clear()
-    execute(level + 1)
+level0 = queue(8)
+level1 = queue(16)
+FCFS = queue(math.inf)
+processes = []
+queues = [level0, level1, FCFS]
 
 
-def checkProcessesLevels():
+def start():
+    processes.sort(key=lambda x: x.arrival)
+    execute(0)
+
+
+def getNextArrival(offset):
     for p in processes:
-        if p.arrival <= Process.t and not queues[p.level].__contains__(p):
-            queues[p.level].append(p)
-
-
-def fcfs():
-    ar = checkNextArrival()
-    for p in queues[len(queues) - 1]:
-        if Process.t + p.burst > ar:
-            p.remB -= (ar - Process.t)
-            execute(0)
+        if p.arrival > Process.t + offset:
+            if not p == Process.nextArrival:
+                Process.nextArrival = p
             break
-        Process.t += p.remB
-        p.remB = 0
-        p.isFinished = True
-        p.finish = Process.t
 
 
 def execute(level):
-    level = max(0, level)
-    if level < len(queues) - 1:
-        roundRobin(level, 8 + level * 8)
-    else:
-        fcfs()
+    isInterrupted = False
+    for p in processes:
+        if p.level == level and not p.isFinished:
+            queues[level].append(p)
+    for p in queues[level]:
+        if p.remB > queues[level].quantum - p.q and Process.nextArrival.arrival > Process.t + queues[level].quantum - p.q:
+            Process.t += queues[level].quantum - p.q
+            p.remB = max(0, p.remB - queues[level].quantum - p.q)
+            p.demote()
+        elif p.remB <= queues[level].quantum - p.q and Process.nextArrival.arrival > Process.t + queues[level].quantum - p.q:
+            Process.t += p.remB
+            p.remB = 0
+            p.isFinished = True
+            p.q = 0
+        elif not level == 0:
+            delta = Process.nextArrival.arrival - Process.t
+            p.remB -= delta
+            Process.t += delta
+            p.q = delta
+            isInterrupted = True
+            break
+        else:
+            Process.t += queues[level].quantum - p.q
+            p.remB = max(0, p.remB - queues[level].quantum - p.q)
+            p.demote()
+            isInterrupted = True
+            break
+    if isInterrupted:
+        Process.nextArrival.assign()
+        interrupt()
+
+
+def interrupt():
+    execute(0)
 
 
 def printAll():
-    print("\n"+"Process no" + " (Arrival time) " + '\t\t' + "Burst " + " Wait " + "Turnaround")
     for p in processes:
-        p.wait()
-        p.print()
-
-
-def checkNextArrival():
-    for p in processes:
-        if p.arrival > Process.t:
-            return p.arrival
-    return math.inf
+        print(str(p.id) + "\t" + str(p.arrival))
 
 
 def test():
@@ -106,8 +111,6 @@ def test():
     Process(0, 26)
     Process(3, 10)
     Process(9, 6)
-
-    execute(0)
 
     printAll()
 
